5751b9cf275b058db2601f11494fca071e168625
FriendLin <522765115@qq.com>
2018-01-12 15:28:49 +0800

    基本实现

---
+++ app/src/main/java/com/lin/dialogs/BaseCommonDialogFmt.java
@@ -0,0 +1,28 @@
+package com.lin.dialogs;
+
+
+import android.app.DialogFragment;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.view.Window;
+
+/**
+ * 基础 dialog
+ */
+
+public class BaseCommonDialogFmt extends DialogFragment {
+    @Override
+    public void onStart() {
+        super.onStart();
+        Window window = getDialog().getWindow();
+        if (window != null) {
+            //设置统一的宽度
+            DisplayMetrics dm = new DisplayMetrics();
+            getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
+            window.setLayout((int) (dm.widthPixels * 0.9), ViewGroup.LayoutParams.WRAP_CONTENT);
+        } else {
+            Log.v("Lin","Dialog error:common");
+        }
+    }
+}
